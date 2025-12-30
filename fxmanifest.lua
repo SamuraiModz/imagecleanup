4200e885e8631837692320b703af423560f911fc
Grafix <139239309+GFX-Code@users.noreply.github.com>
2025-12-30 15:30:22 +0000

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,10 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'SamuraiModz'
-description 'Image cleanup utility for ox_inventory'
+author 'Grafix'
+description 'Image cleanup for ox_inventory'
 version '1.0.0'
 
-server_script 'imagecleanup.lua'
+server_scripts {
+    'imagecleanup.lua',
+}
